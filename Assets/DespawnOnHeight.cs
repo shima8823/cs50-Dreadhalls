32e41e1ec4c7e94fe59cf8ea712f378efb9ec573
shima8823 <taiyaki1275@gmail.com>
2023-05-14 20:12:12 +0900

    first commit

---
+++ Assets/DespawnOnHeight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DespawnOnHeight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < 0)
+        {
+            // Get audio source from WhisperSource object and stop bgm
+            AudioSource bgmSource = GameObject.Find("WhisperSource").GetComponent<AudioSource>();
+            bgmSource.Stop();
+
+            DontDestroy.instance.dungeonFloor = 1;
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
