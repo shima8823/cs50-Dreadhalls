66b0d9b06d87019bcbd99309fd047d7b1f1385d3
shima8823 <taiyaki1275@gmail.com>
2023-05-14 11:41:13 +0900

    first commit

---
+++ Assets/ScoreUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUI : MonoBehaviour
+{
+    public Text score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score.text = "Floor";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = "Floor " + DontDestroy.instance.dungeonFloor;
+    }
+}
